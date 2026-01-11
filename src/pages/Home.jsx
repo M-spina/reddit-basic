22ad0e63a57c5f55096f47c9a22c3deac41f1090
Malcolm Spina <malpoke1@gmail.com>
2026-01-11 20:35:02 +0000

    comment out strictmode for  development remember to add it back when done testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,10 @@ import PostsList from "../features/posts/PostsList.jsx"
 
 
 export default function Home() {
-    const searchTerm = useSelector((state) => state.ui.searchTerm);
-    const dispatch = useDispatch();
     return (
         <div className="page">
             <h2>Welcome to Reddit Basic</h2>
-            <p>This is a simple Reddit client built with React and Vite.</p>
+            <p>This is a simple Reddit client built with React and Vite 2.</p>
             <PostsList />
         </div>
     )
